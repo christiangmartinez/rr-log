9d436a7ef8dd1b9fb513c97420271ee2eab63442
Christian Martinez <cgmartinez1142@gmail.com>
2018-12-27 20:09:59 -0800

    display current date on app start

--- app/src/main/java/com/xtianmartinez/rrlog/ui/MainActivity.java
+++ app/src/main/java/com/xtianmartinez/rrlog/ui/MainActivity.java
@@ -2,14 +2,29 @@ package com.xtianmartinez.rrlog.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.xtianmartinez.rrlog.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+    private TextView currentDate;
+    private Calendar calendar;
+    private SimpleDateFormat dateFormat;
+    private String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendar = Calendar.getInstance();
+        currentDate = (TextView)findViewById(R.id.current_date);
+        dateFormat = new SimpleDateFormat( "MM/dd/yyyy");
+        date = dateFormat.format(calendar.getTime());
+        currentDate.setText(date);
     }
 }
