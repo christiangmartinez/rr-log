04fa883dda321483a1206f872a65e7afa822c850
Christian Martinez <cgmartinez1142@gmail.com>
2018-12-27 19:17:55 -0800

    create initial files

---
+++ app/src/main/java/com/xtianmartinez/rrlog/models/Workout.java
@@ -0,0 +1,4 @@
+package com.xtianmartinez.rrlog.models;
+
+public class Workout {
+}
---
+++ app/src/main/java/com/xtianmartinez/rrlog/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.xtianmartinez.rrlog.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xtianmartinez.rrlog.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
