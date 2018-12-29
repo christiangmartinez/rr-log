7e54784ad2dd64709655e3d8b09f703fcf364f6f
Christian Martinez <cgmartinez1142@gmail.com>
2018-12-28 20:46:36 -0800

    add spinner for pull progressions with toast for selection

--- app/src/main/java/com/xtianmartinez/rrlog/ui/MainActivity.java
+++ app/src/main/java/com/xtianmartinez/rrlog/ui/MainActivity.java
@@ -1,21 +1,21 @@
 package com.xtianmartinez.rrlog.ui;
 
-import android.os.Build;
-import android.support.annotation.RequiresApi;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.WindowManager;
-import android.widget.HorizontalScrollView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.xtianmartinez.rrlog.R;
-
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Toolbar workoutToolbar;
     private TextView currentDate;
     private Calendar calendar;
@@ -28,11 +28,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         calendar = Calendar.getInstance();
-        workoutToolbar = (Toolbar)findViewById(R.id.workout_toolbar);
-        currentDate = (TextView)findViewById(R.id.current_date);
+        workoutToolbar = findViewById(R.id.workout_toolbar);
+        currentDate = (findViewById(R.id.current_date));
         setSupportActionBar(workoutToolbar);
         dateFormat = new SimpleDateFormat( "MM/dd/yyyy");
         date = dateFormat.format(calendar.getTime());
         currentDate.setText(date);
+        Spinner pullSpinner = findViewById(R.id.pull_progressions);
+        ArrayAdapter<CharSequence> pullAdapter = ArrayAdapter.createFromResource(this, R.array.pull_array, android.R.layout.simple_spinner_item);
+        pullAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        pullSpinner.setAdapter(pullAdapter);
+        pullSpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String pullProgression = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(), pullProgression, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
