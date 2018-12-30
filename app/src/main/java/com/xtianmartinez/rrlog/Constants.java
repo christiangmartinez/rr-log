7ff6e57a0e3a7331619e09c7bd39de6dee40562e
Christian Martinez <cgmartinez1142@gmail.com>
2018-12-30 15:22:19 -0800

    change shared preferences key name

--- app/src/main/java/com/xtianmartinez/rrlog/Constants.java
+++ app/src/main/java/com/xtianmartinez/rrlog/Constants.java
@@ -1,5 +1,5 @@
 package com.xtianmartinez.rrlog;
 
 public class Constants {
-    public static final String PREFERENCES_PROGRESSION_KEY = "progression";
+    public static final String PREFERENCES_PULL_PROGRESSION_KEY = "pull-progression";
 }
