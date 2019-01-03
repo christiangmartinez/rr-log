7f3d0037ea362ae136880b2f537d01f5fac45219
Christian Martinez <cgmartinez1142@gmail.com>
2019-01-02 22:33:48 -0800

    add bottom nav and begin fragment refactor

--- app/src/main/java/com/xtianmartinez/rrlog/ui/MainActivity.java
+++ app/src/main/java/com/xtianmartinez/rrlog/ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.xtianmartinez.rrlog.ui;
 
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,47 +26,14 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private static final String TAG = MainActivity.class.getSimpleName();
     private WorkoutViewModel mWorkoutViewModel;
-    private TextView currentDate;
-    private EditText userWeight;
-    private Spinner pullSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: Starting...");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWorkoutViewModel = ViewModelProviders.of(this).get(WorkoutViewModel.class);
 
-        Calendar calendar = Calendar.getInstance();
-        currentDate = findViewById(R.id.current_date);
-        userWeight = findViewById(R.id.user_weight);
-        Button saveWorkout = findViewById(R.id.save_workout);
-
-        Toolbar workoutToolbar = findViewById(R.id.workout_toolbar);
-        setSupportActionBar(workoutToolbar);
-        SimpleDateFormat dateFormat = new SimpleDateFormat( "MM/dd/yyyy");
-        String date = dateFormat.format(calendar.getTime());
-        currentDate.setText(date);
-
-        pullSpinner = findViewById(R.id.pull_progressions);
-        ArrayAdapter<CharSequence> pullAdapter = ArrayAdapter.createFromResource(this, R.array.pull_array, android.R.layout.simple_spinner_item);
-        pullAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        pullSpinner.setAdapter(pullAdapter);
-        pullSpinner.setOnItemSelectedListener(this);
-
-        saveWorkout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "onClick: clicked...");
-                String workoutDate = currentDate.getText().toString();
-                String bodyWeight = userWeight.getText().toString();
-                String pullProgression = pullSpinner.getSelectedItem().toString();
-                boolean validEntry = isValidEntry(bodyWeight);
-                if(!validEntry) return;
-                Workout workout = new Workout();
-                mWorkoutViewModel.insert(workout);
-                Toast.makeText(MainActivity.this, workoutDate + " " + bodyWeight + " " + pullProgression, Toast.LENGTH_LONG).show();
-            }
-        });
     }
 
     @Override
@@ -76,14 +44,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
-    }
-
-    private boolean isValidEntry(String n) {
-        if(n.equals("")) {
-            userWeight.setError("Enter body weight");
-            return false;
-        }
-        return true;
     }
 
 }
---
+++ app/src/main/java/com/xtianmartinez/rrlog/ui/WorkoutFormFragment.java
@@ -0,0 +1,90 @@
+package com.xtianmartinez.rrlog.ui;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.xtianmartinez.rrlog.R;
+import com.xtianmartinez.rrlog.WorkoutViewModel;
+import com.xtianmartinez.rrlog.models.Workout;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import static android.support.constraint.Constraints.TAG;
+
+public class WorkoutFormFragment extends Fragment {
+    private TextView currentDate;
+    private EditText userWeight;
+    private Spinner pullSpinner;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat( "MM/dd/yyyy");
+        String date = dateFormat.format(calendar.getTime());
+        currentDate.setText(date);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =  inflater.inflate(R.layout.fragment_workout_form, container, false);
+
+        Toolbar workoutToolbar = v.findViewById(R.id.workout_toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(workoutToolbar);
+        currentDate = v.findViewById(R.id.current_date);
+        userWeight = v.findViewById(R.id.user_weight);
+        pullSpinner = v.findViewById(R.id.pull_progressions);
+        ArrayAdapter<CharSequence> pullAdapter = ArrayAdapter.createFromResource(this.getActivity(), R.array.pull_array, android.R.layout.simple_spinner_item);
+        pullAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        pullSpinner.setAdapter(pullAdapter);
+//        pullSpinner.setOnItemSelectedListener(this);
+//        Button saveWorkout = v.findViewById(R.id.save_workout);
+//        saveWorkout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Log.d(TAG, "onClick: clicked...");
+//                String workoutDate = currentDate.getText().toString();
+//                String bodyWeight = userWeight.getText().toString();
+//                String pullProgression = pullSpinner.getSelectedItem().toString();
+//                boolean validEntry = isValidEntry(bodyWeight);
+//                if(!validEntry) return;
+//                Workout workout = new Workout();
+//                mWorkoutViewModel.insert(workout);
+//                Toast.makeText(MainActivity.this, workoutDate + " " + bodyWeight + " " + pullProgression, Toast.LENGTH_LONG).show();
+//            }
+//        });
+        return v;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    private boolean isValidEntry(String n) {
+        if(n.equals("")) {
+            userWeight.setError("Enter body weight");
+            return false;
+        }
+        return true;
+    }
+
+
+}
