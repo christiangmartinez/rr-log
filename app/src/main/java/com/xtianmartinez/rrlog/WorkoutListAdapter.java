9fbf8dea8e0b4b65c746a2e223d8bad49005eece
Christian Martinez <cgmartinez1142@gmail.com>
2019-01-13 20:47:34 -0800

    add recyclerview for past workout items

---
+++ app/src/main/java/com/xtianmartinez/rrlog/WorkoutListAdapter.java
@@ -0,0 +1,16 @@
+package com.xtianmartinez.rrlog;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+//public class WorkoutListAdapter extends RecyclerView.Adapter<WorkoutListAdapter.WorkoutViewHolder> {
+//    class WorkoutViewHolder extends RecyclerView.ViewHolder {
+//        private final TextView workoutItemView;
+//
+//        private WorkoutViewHolder(View itemView) {
+//            super(itemView);
+//            workoutItemView = itemView.findViewById(R);
+//        }
+//    }
+//}
