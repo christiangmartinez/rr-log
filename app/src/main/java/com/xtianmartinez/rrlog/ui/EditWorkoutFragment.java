3fedd37741dfa2225a468e23bad63ef466037ba5
Christian Martinez <cgmartinez11142@gmail.com>
2019-06-09 18:29:32 -0700

    add EditWorkoutFragment

---
+++ app/src/main/java/com/xtianmartinez/rrlog/ui/EditWorkoutFragment.java
@@ -0,0 +1,29 @@
+package com.xtianmartinez.rrlog.ui;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.xtianmartinez.rrlog.R;
+
+public class EditWorkoutFragment extends Fragment {
+
+    public EditWorkoutFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_edit_workout, container, false);
+        return v;
+    }
+}
